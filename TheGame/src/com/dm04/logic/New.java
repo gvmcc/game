9e3c0c716e4edb947d415488b82fb2a859229dbe
grego <grego@DESKTOP-R0KHCS0>
2019-11-01 22:40:49 -0400

    two

---
+++ TheGame/src/com/dm04/logic/New.java
@@ -0,0 +1,5 @@
+package com.dm04.logic;
+
+public class New {
+
+}
--- TheGame/src/com/dm04/logic/Think.java
+++ TheGame/src/com/dm04/logic/Think.java
@@ -5,6 +5,6 @@ public class Think {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   
-	}
+	} 
 
 }
